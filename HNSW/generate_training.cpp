031a6f34a4ffd728c01eaf890fa337161f14db2f
Zetta56 <ramonli413@gmail.com>
2024-07-30 11:00:44 -0400

    fix random generation bug in generate_training

--- HNSW/generate_training.cpp
+++ HNSW/generate_training.cpp
@@ -25,19 +25,19 @@ int main() {
         }
     }
 
+    mt19937 gen(config->graph_seed);
+    uniform_real_distribution<float> dis(0, 0.9999999);
     for (int i = 0; i< config->num_training; i++){
-        // Find top 100 nearest neighbors to each node
+        // Choose a random node out of the source dataset
         training[i] = new float[config->dimensions];
-        vector<Edge*> path;
-        pair<int, float*> query = make_pair(i, nodes[i]);
-        vector<pair<float, int>> nearest_neighbors = hnsw->nn_search(config, path, query, config->num_return, true); //finds closest 100 neighbors
+        int index_first = dis(gen) * config->num_nodes;
+        pair<int, float*> query = make_pair(index_first, nodes[index_first]);
 
-        // Choose 2 random nearest neighbors
-        mt19937 gen(config->graph_seed);
-        uniform_int_distribution<> dis(0, 99);
-        static std::random_device rd;
-        int index_second =  nearest_neighbors[dis(gen)].second;
-        int index_third = nearest_neighbors[dis(gen)].second;
+        // Choose 2 random nearest neighbors out of top 100
+        vector<Edge*> path;
+        vector<pair<float, int>> nearest_neighbors = hnsw->nn_search(config, path, query, config->num_return, true);
+        int index_second =  nearest_neighbors[static_cast<int>(dis(gen) * 100)].second;
+        int index_third = nearest_neighbors[static_cast<int>(dis(gen) * 100)].second;
             
         // Choose a random coefficient for each node
         uniform_real_distribution<float> dis_2(0, config->gen_max);
