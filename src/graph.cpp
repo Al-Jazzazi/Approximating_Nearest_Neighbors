bf49f1431565a6f394cd40ee64b0de1d6ba83150
Al-Jazzazi <ya2225@nyu.edu>
2024-11-23 16:07:12 -0500

    fixing traversal for efanna graphs

--- src/graph.cpp
+++ src/graph.cpp
@@ -48,10 +48,10 @@ void Graph::load(Config* config) {
         return;
     }
 
-    // if(  (config->loaded_graph_file.size() > 3) &&   config->loaded_graph_file.substr(config->loaded_graph_file.size() - 3) == "nsg"){
+    if(  (config->loaded_graph_file.size() > 3) &&   config->loaded_graph_file.substr(config->loaded_graph_file.size() - 3) == "nsg"){
         graph_file.read((char *)&width, sizeof(unsigned));
         graph_file.read((char *)&start, sizeof(unsigned));
-    // }
+    }
 
     for (int i = 0; i < num_nodes; ++i) {
         unsigned num_neighbors;
@@ -393,21 +393,28 @@ void Graph::print_k_nodes( Config* config, int k){
 
 
 
-// int main() {
-//     Config* config = new Config();
-//     Graph G(config);
-//     auto start = std::chrono::high_resolution_clock::now();
-//     G.load(config); 
-//     auto end = chrono::high_resolution_clock::now();;
-//     auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-//     cout << "Load time: " << duration / 1000.0 << " seconds, ";
+int main() {
+    Config* config = new Config();
+    Graph G(config);
+    auto start = std::chrono::high_resolution_clock::now();
+    G.load(config); 
+    auto end = chrono::high_resolution_clock::now();;
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    cout << "Load time: " << duration / 1000.0 << " seconds, ";
 
-//     float** queries = new float*[config->num_queries];
-//     load_queries(config, G.nodes, queries);
+    float** queries = new float*[config->num_queries];
+    load_queries(config, G.nodes, queries);
 
-//     G.runQueries(config, queries);
-//     // G.print_100_mappings(config);
-//     // G.print_k_nodes(config);
-//     G.print_k_neigbours(config, 10);
-//     return 0;
-// }
+    G.runQueries(config, queries);
+    G.print_100_mappings(config);
+    // G.print_k_nodes(config);
+    // G.print_k_neigbours(config, 10);
+
+     for (int i = 0; i < config->num_queries; ++i)
+        delete[] queries[i];
+    delete[] queries;
+
+    delete config;
+
+    return 0;
+}
